0455c49e1e96289154276355bbb4877099d9478d
Jakub Plona <plonajakub@gmail.com>
2019-04-03 21:51:47 +0200

    git test

--- Gauss_CUDA/CPU_algorithm.cu
+++ Gauss_CUDA/CPU_algorithm.cu
@@ -1,5 +1,10 @@
 #include <iostream>
 
+
+double* solveLinearSystem(const double **matrixA, const double *matrixB) {
+
+}
+
 int main(int argc, char **argv) {
 	std::cout << "Hello, world!" << std::endl;
 	return 0;
