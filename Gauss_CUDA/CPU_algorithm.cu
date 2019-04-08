881e8cab4c1fa46931cb3edf2cb21dd9762e4a70
Jakub Plona <plonajakub@gmail.com>
2019-04-09 01:41:27 +0200

    solveLinearSystem() takes one concatenated matrix [A|B] from now

--- Gauss_CUDA/CPU_algorithm.cu
+++ Gauss_CUDA/CPU_algorithm.cu
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <exception>
+#include <iomanip>
 
 // For debug purposes
-#define pA printMatrix(matrixA, rank, rank);
-#define pB printMatrix(matrixB, rank, 1);
+#define pAB printMatrix(matrixAB, rankA, rankA + 1)
 
 void printMatrix(double **matrix, int rowNum, int colNum) {
 	std::cout << std::endl;
 	for (int i = 0; i < rowNum; ++i) {
 		std::cout << '[';
 		for (int j = 0; j < colNum - 1; ++j) {
-			std::cout << matrix[i][j] << ',';
+			std::cout << std::setw(4) << matrix[i][j] << ',';
 		}
-		std::cout << matrix[i][colNum - 1] << ']' << std::endl;
+		std::cout << std::setw(4) << matrix[i][colNum - 1] << ']' << std::endl;
 	}
 }
 
@@ -23,111 +23,119 @@ void swap(T &a, T &b) {
 	b = temp;
 }
 
-void solveLinearSystem(double **matrixA, double **matrixB, int rank) {
+// Operations are being conducted in-place
+void solveLinearSystem(double **matrixAB, int rankA) {
 	int baseRowIdx;
 	double columnDivider, subtractCoeff;
-	pA pB	
-	for (int colIdx = 0; colIdx < rank; ++colIdx) {
+	pAB;
+	for (int colIdx = 0; colIdx < rankA; ++colIdx) {
 		// Choose base row
 		baseRowIdx = -1;
-		for (int rowIdx = colIdx; rowIdx < rank; ++rowIdx) {
-			if (matrixA[rowIdx][colIdx] != 0) {
+		for (int rowIdx = colIdx; rowIdx < rankA; ++rowIdx) {
+			if (matrixAB[rowIdx][colIdx] != 0) {
 				baseRowIdx = rowIdx;
 				break;
 			}
 		}
 		if (baseRowIdx == -1) {
-			throw std::invalid_argument("Column can't contain only zeros");
+			throw std::invalid_argument("Column can't contain zeros only");
 		}
-		// Exchange rows if columnDivider isn't matriX[colIdx][colIdx]
+		// Exchange rows if columnDivider isn't matrixAB[colIdx][colIdx]
 		else if (baseRowIdx != colIdx) {
-			for (int exchColIdx = 0; exchColIdx < rank; ++exchColIdx) {
-				swap(matrixA[baseRowIdx][colIdx], matrixA[colIdx][colIdx]);
+			// Could be implemented as parallel operation (vector exchange)
+			for (int exchColIdx = 0; exchColIdx < rankA + 1; ++exchColIdx) {
+				swap(matrixAB[baseRowIdx][exchColIdx], matrixAB[colIdx][exchColIdx]);
 			}
-			swap(matrixB[baseRowIdx][0], matrixA[colIdx][0]);
 			baseRowIdx = colIdx;
 		}
+		pAB;
 		// Chosen row's normalization
-		pA
-		columnDivider = matrixA[baseRowIdx][colIdx];
-		for (int colNormIdx = 0; colNormIdx < rank; ++colNormIdx) {
-			matrixA[baseRowIdx][colNormIdx] /= columnDivider;
-			pA
+		columnDivider = matrixAB[baseRowIdx][colIdx];
+		// Could be implemented as parallel operation (vector by constant division)
+		for (int colNormIdx = 0; colNormIdx < rankA + 1; ++colNormIdx) {
+			matrixAB[baseRowIdx][colNormIdx] /= columnDivider;
+			pAB;
 		}
-		matrixB[baseRowIdx][0] /= columnDivider;
-		pB
 		// Perform row subtraction
-		for (int rowSubIdx = 0; rowSubIdx < rank; ++rowSubIdx) {
-			if (rowSubIdx == baseRowIdx || matrixA[rowSubIdx][colIdx] == 0) {
+		// Could be implemented as parallel operation (multiple vectors subtraction)
+		for (int rowSubIdx = 0; rowSubIdx < rankA; ++rowSubIdx) {
+			if (rowSubIdx == baseRowIdx || matrixAB[rowSubIdx][colIdx] == 0) {
 				continue;
 			}
-			pA
-			subtractCoeff = matrixA[rowSubIdx][colIdx];
-			for (int colSubIdx = 0; colSubIdx < rank; ++colSubIdx) {
-				matrixA[rowSubIdx][colSubIdx] -= subtractCoeff * matrixA[baseRowIdx][colSubIdx];
-				pA
+			pAB;
+			subtractCoeff = matrixAB[rowSubIdx][colIdx];
+			for (int colSubIdx = 0; colSubIdx < rankA + 1; ++colSubIdx) {
+				matrixAB[rowSubIdx][colSubIdx] -= subtractCoeff * matrixAB[baseRowIdx][colSubIdx];
+				pAB;
 			}
-			matrixB[rowSubIdx][0] -= subtractCoeff * matrixB[baseRowIdx][0];
-			pB
 		}
 	}
 }
 
 int main(int argc, char *argv[]) {
 	const int RANK = 4;
-	/*double matrixA[3][3] = {
+	/*double matrixAB[4][4] = {
 		{2, 2, 2, 3},
 		{3, 4, 5, 6},
 		{2, 6, 5, 4},
 		{1, 4, 1, 2}
 	};
-	double matrixB[3][1] = {
+	double matrixB[4][1] = {
 		{2},
 		{4},
 		{2},
 		{2}
 	};*/
 	/*
-		Solution:	-0.6
+		Solution:  -0.6
 					0.1
-					0.6
+				   -0.6
 					1.4
-	
+
 	*/
-	double **matrixA = new double*[RANK];
-	double **matrixB = new double*[RANK];
+	double **matrixAB = new double*[RANK];
 	for (int i = 0; i < RANK; ++i) {
-		matrixA[i] = new double[RANK];
-		matrixB[i] = new double;
+		matrixAB[i] = new double[RANK + 1];
 	}
 
-	matrixA[0][0] = 2;
-	matrixA[0][1] = 2;
-	matrixA[0][2] = 2;
-	matrixA[0][3] = 3;
+	// A
+	matrixAB[0][0] = 2;
+	matrixAB[0][1] = 2;
+	matrixAB[0][2] = 2;
+	matrixAB[0][3] = 3;
 
-	matrixA[1][0] = 3;
-	matrixA[1][1] = 4;
-	matrixA[1][2] = 5;
-	matrixA[1][3] = 6;
+	matrixAB[1][0] = 3;
+	matrixAB[1][1] = 4;
+	matrixAB[1][2] = 5;
+	matrixAB[1][3] = 6;
 
-	matrixA[2][0] = 2;
-	matrixA[2][1] = 6;
-	matrixA[2][2] = 5;
-	matrixA[2][3] = 4;
+	matrixAB[2][0] = 2;
+	matrixAB[2][1] = 6;
+	matrixAB[2][2] = 5;
+	matrixAB[2][3] = 4;
 
-	matrixA[3][0] = 1;
-	matrixA[3][1] = 4;
-	matrixA[3][2] = 1;
-	matrixA[3][3] = 2;
+	matrixAB[3][0] = 1;
+	matrixAB[3][1] = 4;
+	matrixAB[3][2] = 1;
+	matrixAB[3][3] = 2;
 
-	matrixB[0][0] = 2;
-	matrixB[1][0] = 4;
-	matrixB[2][0] = 2;
-	matrixB[3][0] = 2;
+	// B
+	matrixAB[0][4] = 2;
+	matrixAB[1][4] = 4;
+	matrixAB[2][4] = 2;
+	matrixAB[3][4] = 2;
 
-	solveLinearSystem(matrixA, matrixB, RANK);
-	std::cout << std::endl << "Solution vector: ";
-	printMatrix(matrixB, RANK, 1);
+	solveLinearSystem(matrixAB, RANK);
+	std::cout << std::endl << "Solution vector:" << std::endl;
+	std::cout << '[' << std::setw(4) << matrixAB[0][RANK] << std::endl;
+	for (int i = 1; i < RANK - 1; ++i) {
+		std::cout << std::setw(5) << matrixAB[i][RANK] << std::endl;
+	}
+	std::cout << std::setw(5) << matrixAB[RANK - 1][RANK] << " ]" << std::endl;
+
+	for (int i = 0; i < RANK; ++i) {
+		delete[] matrixAB[i];
+	}
+	delete[] matrixAB;
 	return 0;
 }
