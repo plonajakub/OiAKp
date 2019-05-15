185ce775c4be0f222382d3bce41851750f821727
Jakub Plona <plonajakub@gmail.com>
2019-05-16 00:06:22 +0200

    GPU algorithm uses shared memory from now

--- Gauss_CUDA/GPU_algorithm.cu
+++ Gauss_CUDA/GPU_algorithm.cu
@@ -9,48 +9,57 @@
 #include "GPU_algorithm.cuh"
 
 #define getEl(matrix, type, pitch, row, col) (*((type*)((char*)matrix + pitch * row + sizeof(type) * col)))
+#define defaultThreadsPerBlock (32)
 
 __global__ void swapMatrixRowsParallel(double *matrix, size_t pitch, int matrixDimX, int rowIdx1, int rowIdx2) {
 	int colIdx = blockIdx.x * blockDim.x + threadIdx.x;
+	extern __shared__ double row[];
 	if (colIdx < matrixDimX) {
-		double &el1 = getEl(matrix, double, pitch, rowIdx1, colIdx);
-		double &el2 = getEl(matrix, double, pitch, rowIdx2, colIdx);
-		double temp = el1;
-		el1 = el2;
-		el2 = temp;
+		row[threadIdx.x] = getEl(matrix, double, pitch, rowIdx1, colIdx);
+		getEl(matrix, double, pitch, rowIdx1, colIdx) = getEl(matrix, double, pitch, rowIdx2, colIdx);
+		getEl(matrix, double, pitch, rowIdx2, colIdx) = row[threadIdx.x];
 	}
 }
 
 __global__ void divideMatrixRowByConstantParallel(double *matrix, size_t pitch, int matrixDimX, int rowIdx, double divisor) {
 	int colIdx = blockIdx.x * blockDim.x + threadIdx.x;
+	extern __shared__ double row[];
 	if (colIdx < matrixDimX) {
-		getEl(matrix, double, pitch, rowIdx, colIdx) /= divisor;
+		row[threadIdx.x] = getEl(matrix, double, pitch, rowIdx, colIdx);
+		row[threadIdx.x] /= divisor;
+		getEl(matrix, double, pitch, rowIdx, colIdx) = row[threadIdx.x];
 	}
 }
 
 __global__ void subtracMatrixRows(double *matrix, size_t pitch, int matrixDimX, int subtrahendRowIdx, int minuendRowIdx, double subtractCoeff) {
 	int colIdx = blockIdx.x * blockDim.x + threadIdx.x;
+	extern __shared__ double s[];
 	if (colIdx < matrixDimX) {
-		double &subtrahend = getEl(matrix, double, pitch, subtrahendRowIdx, colIdx);
-		double &minuend = getEl(matrix, double, pitch, minuendRowIdx, colIdx);
-		minuend -= subtractCoeff * subtrahend;
+		double *minRow = s, *subRow = s + blockDim.x;
+		minRow[threadIdx.x] = getEl(matrix, double, pitch, minuendRowIdx, colIdx);
+		subRow[threadIdx.x] = getEl(matrix, double, pitch, subtrahendRowIdx, colIdx); // Conflicts in memory can occur here
+		minRow[threadIdx.x] -= subtractCoeff * subRow[threadIdx.x];
+		getEl(matrix, double, pitch, minuendRowIdx, colIdx) = minRow[threadIdx.x];
 	}
 }
 
 __global__ void performMatrixVerticalRowSubtraction(double *matrix, size_t pitch, int matrixDimX, int matrixDimY, int baseRowIdx, int baseColIdx) {
 	int rowIdx = blockIdx.x * blockDim.x + threadIdx.x;
-	if (rowIdx < matrixDimY && rowIdx != baseRowIdx && getEl(matrix, double, pitch, rowIdx, baseColIdx) != 0) {
-		double subtractCoeff = getEl(matrix, double, pitch, rowIdx, baseColIdx);
-		const int threadsPerBlock = 32;
-		const int blocksPerGridDimX = ceil((matrixDimX) / (double)threadsPerBlock);
-		cudaStream_t stream;
-		cudaStreamCreateWithFlags(&stream, cudaStreamNonBlocking);
-		subtracMatrixRows << <blocksPerGridDimX, threadsPerBlock, 0, stream >> > (matrix, pitch, matrixDimX, baseRowIdx, rowIdx, subtractCoeff);
-		cudaStreamDestroy(stream);
+	extern __shared__ double subtracCoeffs[];
+	if (rowIdx < matrixDimY && rowIdx != baseRowIdx) {
+		subtracCoeffs[threadIdx.x] = getEl(matrix, double, pitch, rowIdx, baseColIdx);
+		if (subtracCoeffs[threadIdx.x] != 0) {
+			const int blocksPerGridDimX = ceil((matrixDimX) / (double)defaultThreadsPerBlock);
+			cudaStream_t stream;
+			cudaStreamCreateWithFlags(&stream, cudaStreamNonBlocking);
+			subtracMatrixRows<<<blocksPerGridDimX, defaultThreadsPerBlock, 2 * defaultThreadsPerBlock * sizeof(double), stream>>>
+				(matrix, pitch, matrixDimX, baseRowIdx, rowIdx, subtracCoeffs[threadIdx.x]);
+			cudaStreamDestroy(stream);
+		}
 	}
 }
 
-__global__ void solveLinearSystemKernel(int degreeOfMatrixA, size_t pitch, double *matrixAB, Result &result_callback) {
+__global__ void solveLinearSystemKernel(int degreeOfMatrixA, size_t pitch, double *matrixAB, Result &outputData) {
 
 	const int threadsPerBlock = 32;
 	const int blocksPerGridDimX = ceil((degreeOfMatrixA + 1) / (double)threadsPerBlock);
@@ -71,13 +80,14 @@ __global__ void solveLinearSystemKernel(int degreeOfMatrixA, size_t pitch, doubl
 			}
 		}
 		if (baseRowIdx == -1) {
-			result_callback = Result::ALGORITHM_ERROR;
+			outputData = Result::ALGORITHM_ERROR;
 			return;
 		}
 		// Exchange rows if columnDivider isn't matrixAB[colIdx][colIdx]
 		else if (baseRowIdx != colIdx) {
 			// Exchange rows (vectors)
-			swapMatrixRowsParallel << <blocksPerGridDimX, threadsPerBlock >> > (matrixAB, pitch, degreeOfMatrixA + 1, baseRowIdx, colIdx);
+			swapMatrixRowsParallel <<<blocksPerGridDimX, threadsPerBlock, 2 * threadsPerBlock * sizeof(double)>>>
+				(matrixAB, pitch, degreeOfMatrixA + 1, baseRowIdx, colIdx);
 			baseRowIdx = colIdx;
 		}
 
@@ -87,12 +97,14 @@ __global__ void solveLinearSystemKernel(int degreeOfMatrixA, size_t pitch, doubl
 		columnDivider = getEl(matrixAB, double, pitch, baseRowIdx, colIdx);
 
 		// Divide row (vector) by constant
-		divideMatrixRowByConstantParallel << <blocksPerGridDimX, threadsPerBlock >> > (matrixAB, pitch, degreeOfMatrixA + 1, baseRowIdx, columnDivider);
+		divideMatrixRowByConstantParallel<<<blocksPerGridDimX, threadsPerBlock, threadsPerBlock * sizeof(double)>>>
+			(matrixAB, pitch, degreeOfMatrixA + 1, baseRowIdx, columnDivider);
 
 		cudaDeviceSynchronize();
 
 		// Perform multiple rows (vectors) subtraction
-		performMatrixVerticalRowSubtraction << <blocksPerGridDimY, threadsPerBlock >> > (matrixAB, pitch, degreeOfMatrixA + 1, degreeOfMatrixA, baseRowIdx, colIdx);
+		performMatrixVerticalRowSubtraction<<<blocksPerGridDimY, threadsPerBlock, threadsPerBlock * sizeof(double)>>>
+			(matrixAB, pitch, degreeOfMatrixA + 1, degreeOfMatrixA, baseRowIdx, colIdx);
 	}
 }
 
@@ -100,12 +112,18 @@ gpu_info solveLinearSystemParallel(int degreeOfMatrixA, double **matrixAB) {
 	// Error code holder
 	cudaError_t cudaStatus;
 
-	gpu_info info = { Result::SUCCESS, 0.0 }; 
+	// Configuration of shared memory banks' size
+	cudaDeviceSetSharedMemConfig(cudaSharedMemBankSizeEightByte);
+
+	// Returned data
+	gpu_info info = {Result::SUCCESS, 0.0};
+	
+	// Algorithm's work time
 	cudaEvent_t start, stop;
 	cudaEventCreate(&start);
 	cudaEventCreate(&stop);
 
-	// Device memory allocation
+	// Device memory allocation (pitched memory)
 	double *devMatrixAB;
 	size_t pitch;
 	cudaStatus = cudaMallocPitch(&devMatrixAB, &pitch,
@@ -113,6 +131,7 @@ gpu_info solveLinearSystemParallel(int degreeOfMatrixA, double **matrixAB) {
 
 	if (cudaStatus != cudaSuccess) {
 		std::cout << "cudaMallocPitch failed!" << std::endl;
+		std::cout << "Error description: " << cudaGetErrorString(cudaStatus) << std::endl;
 		info.result = Result::CUDA_ERROR;
 		goto Cleanup;
 	}
@@ -124,33 +143,28 @@ gpu_info solveLinearSystemParallel(int degreeOfMatrixA, double **matrixAB) {
 
 		if (cudaStatus != cudaSuccess) {
 			std::cout << "cudaMemcpy failed!" << std::endl;
+			std::cout << "Error description: " << cudaGetErrorString(cudaStatus) << std::endl;
 			info.result = Result::CUDA_ERROR;
 			goto Cleanup;
 		}
 	}
 
-	//cudaProfilerStart();
 	//start time measurement
 	cudaEventRecord(start);
 
 	// Kernel launch
-	solveLinearSystemKernel << <1, 1 >> > (degreeOfMatrixA, pitch, devMatrixAB, info.result); 
+	solveLinearSystemKernel<<<1, 1>>>(degreeOfMatrixA, pitch, devMatrixAB, info.result); 
 
 	cudaStatus = cudaGetLastError();
 	if (cudaStatus != cudaSuccess) {
 		std::cout << "solveLinearSystemKernel failed: " << cudaGetErrorString(cudaStatus) << std::endl;
+		std::cout << "Error description: " << cudaGetErrorString(cudaStatus) << std::endl;
 		info.result = Result::CUDA_ERROR;
 		goto Cleanup;
 	}
 
 	// Wait for device to complete the work
 	cudaStatus = cudaDeviceSynchronize();
-
-	//stop time measurement
-	cudaEventRecord(stop);
-	cudaEventSynchronize(stop);
-	cudaEventElapsedTime(&info.time, start, stop);
-	//cudaProfilerStop();
 
 	if (cudaStatus != cudaSuccess) {
 		std::cout << "cudaDeviceSynchronize returned error code " << cudaStatus <<
@@ -159,8 +173,13 @@ gpu_info solveLinearSystemParallel(int degreeOfMatrixA, double **matrixAB) {
 		goto Cleanup;
 	}
 
+	//stop time measurement
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&info.time, start, stop);
+
 	if (info.result == Result::ALGORITHM_ERROR) {
-		std::cout << "Error during solving the linear system" << std::endl;
+		std::cout << "Provided linear system hasn't proper format!" << std::endl;
 		goto Cleanup;
 	}
 
@@ -171,6 +190,7 @@ gpu_info solveLinearSystemParallel(int degreeOfMatrixA, double **matrixAB) {
 
 		if (cudaStatus != cudaSuccess) {
 			std::cout << "cudaMemcpy failed!" << std::endl;
+			std::cout << "Error description: " << cudaGetErrorString(cudaStatus) << std::endl;
 			info.result = Result::CUDA_ERROR;
 			goto Cleanup;
 		}
@@ -184,78 +204,25 @@ Cleanup:
 	cudaStatus = cudaDeviceReset();
 	if (cudaStatus != cudaSuccess) {
 		std::cout << "cudaDeviceReset failed!" << std::endl;
+		std::cout << "Error description: " << cudaGetErrorString(cudaStatus) << std::endl;
 		info.result = Result::CUDA_ERROR;
 	}
 	return info;
 }
 
-/*
+
 int main(int argc, char *argv[]) {
-
-	// Matrix size is MATRIX_DEGREE x (MATRIX_DEGREE + 1)
-	const int MATRIX_DEGREE = 4;
-
-	/*double matrixAB[4][4] = {
-		{2, 2, 2, 3},
-		{3, 4, 5, 6},
-		{2, 6, 5, 4},
-		{1, 4, 1, 2}
-	};
-	double matrixB[4][1] = {
-		{2},
-		{4},
-		{2},
-		{2}
-	};*/
-	/*
-		Solution:  -0.6
-					0.1
-				   -0.6
-					1.4
-
-	*/
+	const int MATRIX_DEGREE = 5;
 
 	// Host allocation
-	/*
-	double **matrixAB = new double*[MATRIX_DEGREE];
-	for (int i = 0; i < MATRIX_DEGREE; ++i) {
-		matrixAB[i] = new double[MATRIX_DEGREE + 1];
-	}
-
-	// A
-	matrixAB[0][0] = 2;
-	matrixAB[0][1] = 2;
-	matrixAB[0][2] = 2;
-	matrixAB[0][3] = 3;
-
-	matrixAB[1][0] = 3;
-	matrixAB[1][1] = 4;
-	matrixAB[1][2] = 5;
-	matrixAB[1][3] = 6;
-
-	matrixAB[2][0] = 2;
-	matrixAB[2][1] = 6;
-	matrixAB[2][2] = 5;
-	matrixAB[2][3] = 4;
-
-	matrixAB[3][0] = 1;
-	matrixAB[3][1] = 4;
-	matrixAB[3][2] = 1;
-	matrixAB[3][3] = 2;
-
-	// B
-	matrixAB[0][4] = 2;
-	matrixAB[1][4] = 4;
-	matrixAB[2][4] = 2;
-	matrixAB[3][4] = 2;
+	double **matrixAB = nullptr;
+	Utils::GenMatrix(&matrixAB, MATRIX_DEGREE, 0);
 
 	// Create copy of matrixAB
-	double **matrixAB_copy = new double*[MATRIX_DEGREE];
-	for (int i = 0; i < MATRIX_DEGREE; ++i) {
-		matrixAB_copy[i] = new double[MATRIX_DEGREE + 1];
-		std::memcpy(matrixAB_copy[i], matrixAB[i], (MATRIX_DEGREE + 1) * sizeof(double));
-	}
+	double **matrixAB_copy = nullptr;
+	matrixAB_copy = Utils::DuplicateMatrix(matrixAB, MATRIX_DEGREE, MATRIX_DEGREE + 1);
 
+	// Print generated matrix
 	std::cout << "Matrix [A|B]:" << std::endl;
 	Utils::printMatrix(matrixAB, MATRIX_DEGREE, MATRIX_DEGREE + 1);
 	std::cout << std::endl;
@@ -272,10 +239,10 @@ int main(int argc, char *argv[]) {
 	}
 
 	// Solve linear system on GPU
-	Result result;
-	result = solveLinearSystemParallel(MATRIX_DEGREE, matrixAB);
+	gpu_info info;
+	info = solveLinearSystemParallel(MATRIX_DEGREE, matrixAB);
 
-	switch (result) {
+	switch (info.result) {
 	case SUCCESS:
 		std::cout << "solverLinearSystemParallel() returned without errors!" << std::endl;
 		break;
@@ -296,6 +263,7 @@ int main(int argc, char *argv[]) {
 
 	Utils::printSolutionVectorFromMatrix(MATRIX_DEGREE, matrixAB);
 	std::cout << std::endl;
+
 	if (Utils::checkLSSolution(MATRIX_DEGREE, matrixAB_copy, matrixAB)) {
 		std::cout << "Solution vector is correct!" << std::endl;
 	}
@@ -303,15 +271,12 @@ int main(int argc, char *argv[]) {
 		std::cout << "Solution vector is incorrect! (small floating-point operations' errors are possible)" << std::endl;
 		std::cout << "Error: " << Utils::getLSSolutionError(MATRIX_DEGREE, matrixAB_copy, matrixAB) << std::endl;
 	}
+	
+	std::cout << "Calculations time: " << info.time << " ms" << std::endl;
 
 	// Host memory cleanup
-	for (int i = 0; i < MATRIX_DEGREE; ++i) {
-		delete[] matrixAB[i];
-		delete matrixAB_copy[i];
-	}
-	delete[] matrixAB;
-	delete[] matrixAB_copy;
+	Utils::DeleteMatrix(matrixAB, MATRIX_DEGREE);
+	Utils::DeleteMatrix(matrixAB_copy, MATRIX_DEGREE);
 
 	return 0;
 }
-/**/
